9e9ba917ceb82554f48b84475d812e36e8fe370f
Lukash <89802614+AlexanderLukash@users.noreply.github.com>
2022-07-07 14:18:39 +0300

    Update settings.py

--- django_movie/settings.py
+++ django_movie/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-=8ju^^609f8)6)=hn21bb&45=te^*a(aevw9_+l6zik5+77t^2'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
