8f2ae945032958809d2904b95d9ac34fad840e94
Lukash <89802614+AlexanderLukash@users.noreply.github.com>
2022-07-09 13:35:46 +0300

    Update urls.py

--- django_movie/urls.py
+++ django_movie/urls.py
@@ -23,7 +23,4 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('ckeditor/', include('ckeditor_uploader.urls')),
     path("", include("movies.urls")),
-]
-
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
